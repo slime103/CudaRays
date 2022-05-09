0576fbd70db579956f7e78c7295a5b5e5cca197c
Slime103 <zes888@gmail.com>
2022-05-08 23:39:04 -0400

    broken shutter settings

--- src/Main.cu
+++ src/Main.cu
@@ -33,7 +33,7 @@ struct material
 	glm::vec3 specular;
 	float power;
 
-	__device__ material(float r1, float g1, float b1, float r2, float g2, float b2, float r3, float g3, float b3, float p)
+	material(float r1, float g1, float b1, float r2, float g2, float b2, float r3, float g3, float b3, float p)
 	{
 		diffuse.r = r1;
 		diffuse.g = g1;
@@ -63,6 +63,12 @@ struct TriangleMesh
 	material* mat;
 	int numOfTris = 0;
 	face* tris = nullptr;
+
+	//movement
+	bool doesMove = false;
+	float startMoveTime = 0.f;
+	float endMoveTime = 0.f;
+	glm::vec3 transformation;
 };
 
 struct sphere
@@ -231,10 +237,11 @@ __device__ inline bool triangleTest(glm::vec3 V, glm::vec3 W, glm::vec3 a, glm::
 {
 	//	 A
 	// B	C
-	//compute N in a counter clockwise manner, since the Vertex normals may not be flat shaded
+	//compute N in a counter clockwise manner
 	glm::vec3 ab = b - a;
 	glm::vec3 ac = c - a;
 	glm::vec3 N = cross(ab, ac);
+	
 	float d = -dot(N, a);
 	glm::vec4 H = glm::vec4(N.x, N.y, N.z, d);
 
@@ -281,24 +288,42 @@ __device__ inline bool triangleTest(glm::vec3 V, glm::vec3 W, glm::vec3 a, glm::
 }
 
 /*Returns a point P of the closest triangle along with the normal direction at point P for shading*/
-__device__ inline bool testTriangles(glm::vec3 V, glm::vec3 W, glm::vec3 &P, glm::vec3 &N, VertexData* vertData, TriangleMesh* trimesh, int numOfMeshes)
+__device__ inline bool testTriangles(glm::vec3 V, glm::vec3 W, glm::vec3 &P, glm::vec3 &N, VertexData* vertData, TriangleMesh* trimesh, int numOfMeshes, 
+material **mat, const float cTime)
 {
 	bool hit = false;
 	float z = 1000.f;
 	int meshOffset = 0;
 	int triOffset = 0;
 	float u,v;
+	glm::vec3 transformation;
 
 	//loop through all triangles in the scene
 	for (int k = 0; k < numOfMeshes; k++)
 	{
+		//does this obj move? Is the current time within the objects movement interval?
+		if ((trimesh + k)->doesMove)
+		{
+			if (cTime <= (trimesh + k)->endMoveTime && cTime >= (trimesh + k)->startMoveTime)
+			{
+				//find the 0-1 value of the current time relative to the objects transformation duration
+				float amount = (cTime - (trimesh + k)->startMoveTime) / ((trimesh + k)->endMoveTime - (trimesh + k)->startMoveTime);
+				transformation = (trimesh + k)->transformation * amount;
+			}
+		}
+		else
+		{
+			transformation = glm::vec3(0.);
+		}
+
 		for (int i = 0; i < trimesh->numOfTris; i++)
 		{
 			//note: vertex indexes begin at 1 for obj files, thus the -1
 			
 			//did we hit something?
-			if (triangleTest(V, W, *(vertData->verts + ((trimesh + k)->tris + i)->x - 1), *(vertData->verts + ((trimesh + k)->tris + i)->y - 1),
-				*(vertData->verts + ((trimesh + k)->tris + i)->z - 1), u, v, P))
+			if (triangleTest(V, W, *(vertData->verts + ((trimesh + k)->tris + i)->x - 1) + transformation,
+				*(vertData->verts + ((trimesh + k)->tris + i)->y - 1) + transformation,
+				*(vertData->verts + ((trimesh + k)->tris + i)->z - 1) + transformation, u, v, P))
 			{
 				float dist = glm::distance(V, P);
 				if (dist < z) //is this point closer to the camera? 
@@ -330,6 +355,118 @@ __device__ inline bool testTriangles(glm::vec3 V, glm::vec3 W, glm::vec3 &P, glm
 			N = *(vertData->vnorms + ((trimesh + meshOffset)->tris + triOffset)->vn1 - 1);
 		}
 		//TODO: Calculate Texture coordinates
+
+		//set the material ptr for shading
+		*mat = (trimesh + meshOffset)->mat;
+	}
+
+	//shade the closest point if we hit something
+	return hit;
+}
+
+/*Returns true if any triangle is hit from V to d*/
+__device__ inline bool testTrianglesAny(glm::vec3 V, glm::vec3 W, float d, VertexData* vertData, TriangleMesh* trimesh, int numOfMeshes, const float cTime)
+{
+	//dummy variables
+	glm::vec3 P;
+	float u, v;
+
+	glm::vec3 transformation;
+
+	//loop through all triangles in the scene
+	for (int k = 0; k < numOfMeshes; k++)
+	{
+		//does this obj move? Is the current time within the objects movement interval?
+		if ((trimesh + k)->doesMove)
+		{
+			if (cTime <= (trimesh + k)->endMoveTime && cTime >= (trimesh + k)->startMoveTime)
+			{
+				//find the 0-1 value of the current time relative to the objects transformation duration
+				float amount = (cTime - (trimesh + k)->startMoveTime) / ((trimesh + k)->endMoveTime - (trimesh + k)->startMoveTime);
+				transformation = (trimesh + k)->transformation * amount;
+			}
+		}
+		else
+		{
+			transformation = glm::vec3(0.);
+		}
+
+		for (int i = 0; i < trimesh->numOfTris; i++)
+		{
+			//vertex indexes begin at 1 for obj files, thus the -1
+			//Did we hit something?
+			if (triangleTest(V, W, *(vertData->verts + ((trimesh + k)->tris + i)->x - 1) + transformation,
+				*(vertData->verts + ((trimesh + k)->tris + i)->y - 1) + transformation,
+				*(vertData->verts + ((trimesh + k)->tris + i)->z - 1) + transformation, u, v, P))
+			{
+				//is it between V and d ?
+				float dist = length(P - V);
+				if (dist <= d && dist > 0.000001) //triangle should not be clipping
+				{
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+/*Returns a point P of the closest triangle along with the normal direction at point P for shading*/
+__device__ inline bool testTriangles(glm::vec3 V, glm::vec3 W, glm::vec3& P, glm::vec3& N, VertexData* vertData, TriangleMesh* trimesh, int numOfMeshes,
+	material** mat)
+{
+	bool hit = false;
+	float z = 1000.f;
+	int meshOffset = 0;
+	int triOffset = 0;
+	float u, v;
+
+	//loop through all triangles in the scene
+	for (int k = 0; k < numOfMeshes; k++)
+	{
+		for (int i = 0; i < trimesh->numOfTris; i++)
+		{
+			//note: vertex indexes begin at 1 for obj files, thus the -1
+
+			//did we hit something?
+			if (triangleTest(V, W, *(vertData->verts + ((trimesh + k)->tris + i)->x - 1),
+				*(vertData->verts + ((trimesh + k)->tris + i)->y - 1),
+				*(vertData->verts + ((trimesh + k)->tris + i)->z - 1), u, v, P))
+			{
+				float dist = glm::distance(V, P);
+				if (dist < z) //is this point closer to the camera? 
+				{
+					//save offsets to set normal data later
+					meshOffset = k;
+					triOffset = i;
+
+					//we are closer so update the z position
+					z = dist;
+				}
+				hit = true;
+			}
+		}
+	}
+
+	//if the mesh is flat shading just pass in v1 vertex normal
+	//otherwise use u and v to calculate the smooth shaded normal at point P
+	if (hit)
+	{
+		if ((trimesh + meshOffset)->isSmooth)
+		{
+			N = (1 - u - v) * *(vertData->vnorms + ((trimesh + meshOffset)->tris + triOffset)->vn1 - 1)
+				+ u * *(vertData->vnorms + ((trimesh + meshOffset)->tris + triOffset)->vn2 - 1)
+				+ v * *(vertData->vnorms + ((trimesh + meshOffset)->tris + triOffset)->vn3 - 1);
+		}
+		else
+		{
+			N = *(vertData->vnorms + ((trimesh + meshOffset)->tris + triOffset)->vn1 - 1);
+		}
+		//TODO: Calculate Texture coordinates
+
+		//set the material ptr for shading
+		*mat = (trimesh + meshOffset)->mat;
 	}
 
 	//shade the closest point if we hit something
@@ -350,7 +487,8 @@ __device__ inline bool testTrianglesAny(glm::vec3 V, glm::vec3 W, float d, Verte
 		{
 			//vertex indexes begin at 1 for obj files, thus the -1
 			//Did we hit something?
-			if (triangleTest(V, W, *(vertData->verts + ((trimesh + k)->tris + i)->x - 1), *(vertData->verts + ((trimesh + k)->tris + i)->y - 1),
+			if (triangleTest(V, W, *(vertData->verts + ((trimesh + k)->tris + i)->x - 1),
+				*(vertData->verts + ((trimesh + k)->tris + i)->y - 1),
 				*(vertData->verts + ((trimesh + k)->tris + i)->z - 1), u, v, P))
 			{
 				//is it between V and d ?
@@ -366,7 +504,8 @@ __device__ inline bool testTrianglesAny(glm::vec3 V, glm::vec3 W, float d, Verte
 	return false;
 }
 
-__device__ glm::vec3 shadePoint(glm::vec3 P, glm::vec3 W, glm::vec3 N, VertexData* vertData, TriangleMesh* trimesh, material* m, pointLight* lights, int num_lights, int numOfMeshes)
+__device__ glm::vec3 shadePoint(glm::vec3 P, glm::vec3 W, glm::vec3 N, VertexData* vertData, TriangleMesh* trimesh, material* m, 
+pointLight* lights, int num_lights, int numOfMeshes)
 {
 	glm::vec3 c = glm::vec3(0.f);
 
@@ -416,8 +555,9 @@ __device__ inline glm::vec3 raySpheres(float tMin, glm::vec3 V, glm::vec3 W, sph
 	return color;
 }*/
 
-struct Camera
+class Camera
 {
+	public:
 	//set the film size
 	const float image_plane_width = 0.07f; // 7cm
 	const float image_plane_height = 0.06f; // 6cm
@@ -425,9 +565,13 @@ struct Camera
 
 	//select a focal length, aperature, and focus distance
 	const float focal_length = 0.05f; // 50mm
-	const float aperture = 16.f; // f1.7
-	const float focus_dist = 2.5f; // 2.5m
+	const float aperture = 16.f;
+	const float focus_dist = 1.5f;
 	const float lens_diameter = focal_length / aperture;
+
+	const float shutterSpeed = 1.f; //1/100th of a sec
+
+	//__device__ Camera camera();
 };
 
 //Returns a point on a concentric disk, takes x and y values between -1 and 1
@@ -486,6 +630,7 @@ __device__ inline void dofRay(glm::vec3 &V, glm::vec3 &W, const Camera& camera, 
 __device__ inline glm::vec3 
 shadePoint(const Camera &camera, const float pixel_x, const float pixel_y, VertexData* vertData, TriangleMesh* trimesh, int numOfMeshes, curandState_t* state)
 {
+	//return glm::vec3(0.5f,0.5f,0.5f);/*
 	//find this pixel's camera space position
 	glm::vec3 pixelpos;
 	pixelpos.x = pixel_x / blockDim.x;
@@ -505,19 +650,19 @@ shadePoint(const Camera &camera, const float pixel_x, const float pixel_y, Verte
 
 	// TEMP DEFINE LIGHT DATA
 	const int num_lights = 2;
-	pointLight lights[num_lights] = { pointLight(0.5f, 5.5f, -5.5f, .59f, .93f, .59f, 25.f), pointLight(.43f, .15f, -4.2f, .19f, .63f, .49f, 1.f) };
-	// TEMP DEFINE MATERIAL DATA
-	material sphere_mat = material(0.1f, 0.1f, 0.1f, 0.2f, 0.4f, 0.5f, 1.f, 1.f, 1.f, 1.5f);
+	pointLight lights[num_lights] = { pointLight(0.5f, 5.5f, -5.5f, .59f, .93f, .59f, 10.f), pointLight(.53f, -.6f, -4.2f, .19f, .63f, .49f, .5f) };	
 
 	glm::vec3 P;
 	glm::vec3 N;
+	material *mat = nullptr;
 
-	if (testTriangles(V, W, P, N, vertData, trimesh, numOfMeshes))
+	if (testTriangles(V, W, P, N, vertData, trimesh, numOfMeshes, &mat))
 	{
-		color = shadePoint(P, W, N, vertData, trimesh, &sphere_mat, &lights[0], num_lights, numOfMeshes);
+		color = shadePoint(P, W, N, vertData, trimesh, mat, &lights[0], num_lights, numOfMeshes);
 	}
 
 	return color;
+	//*/
 }
 
 __global__ void shadePixels(glm::vec3* pixelptr, VertexData* vertData, TriangleMesh* trimesh, int numOfMeshes)
@@ -538,27 +683,39 @@ __global__ void shadePixels(glm::vec3* pixelptr, VertexData* vertData, TriangleM
 	//SET PIXEL SAMPLES HERE
 	const int num_pixel_samples = 3; //this number will be squared
 
-	int cell_size = 1 / num_pixel_samples;
+	const int cell_size = 1 / num_pixel_samples;
 
 	curandState_t state;
 	curand_init(2127 + tid, tid, 0, &state);
 	glm::vec3 color = glm::vec3(0.);
 
-	for (int i = 0; i < num_pixel_samples; i++)
-	{
-		//divide the pixel further into cells
-		//Choose a random point from wihtin each cell
-		for (int j = 0; j < num_pixel_samples; j++)
-		{
-			float rand_x = pixel_x + cell_size * i + curand_uniform(&state);
-			float rand_y = pixel_y + cell_size * j + curand_uniform(&state);
+	//const int num_shutter_samples = 3;
+	//const float shutter_interval = (1.f / num_shutter_samples) * 1.f; //TEMP VAR - can't access the camera here?!?
+	
+	//Shutter sampling
+	//for (int k = 0; k < num_shutter_samples; k++)
+	//{
+		//pick a random point within one of the intervals
+		//float rand_time = (1.f / num_shutter_samples) * 1.f * k * curand_uniform(&state);
 
-			color += shadePoint(camera, rand_x, rand_y, vertData, trimesh, numOfMeshes, &state);
-		}	
-	}
+		//pixel sampling
+		for (int i = 0; i < num_pixel_samples; i++)
+		{
+			//divide the pixel further into cells
+			//Choose a random point from wihtin each cell
+			for (int j = 0; j < num_pixel_samples; j++)
+			{
+				float rand_x = pixel_x + cell_size * i + curand_uniform(&state);
+				float rand_y = pixel_y + cell_size * j + curand_uniform(&state);
+
+				color += shadePoint(camera, rand_x, rand_y, vertData, trimesh, numOfMeshes, &state);
+				//color += glm::vec3(rand_time, rand_time, rand_time);
+			}	
+		}
+	//}
 
 	//average the pixel sample colors
-	color /= (num_pixel_samples * num_pixel_samples);
+	color /= (num_pixel_samples * num_pixel_samples /* * num_shutter_samples*/);
 
 	//color = shadePoint(camera, pixel_x, pixel_y, vertData, trimesh, numOfMeshes);
 
@@ -606,8 +763,8 @@ int main()
 	TriangleMesh* trimeshes;
 	VertexData* vertData;
 	const int maxMeshes = 5;
-	const int maxVertices = 300; //For the entire scene
-	const int maxFaces = 300; //Per object
+	const int maxVertices = 50; //For the entire scene
+	const int maxFaces = 30; //Per object
 	
 	cudaMallocManaged(&trimeshes, sizeof(TriangleMesh) * maxMeshes);
 	cudaMallocManaged(&vertData, sizeof(vertData));
@@ -628,7 +785,32 @@ int main()
 		
 		(trimeshes + i)->tris = tris;
 	}
-	
+
+	//set up materials
+	material *materials;
+	int numOfMats = 4;
+	cudaMallocManaged(&materials, sizeof(material) * numOfMats);
+
+	//TEMP initialize the data
+	*materials = material(0.1f, 0.1f, 0.1f, 0.2f, 0.4f, 0.5f, 1.f, 1.f, 1.f, 1.5f);
+	*(materials+1) = material(0.1f, 0.55f, 0.1f, 0.6f, 0.6f, 0.5f, 1.f, 1.f, 1.f, 5.f);
+	*(materials+2) = material(0.8f, 0.6f, 0.4f, 0.3f, 0.2f, 0.3f, 1.f, 1.f, 1.f, 2.f);
+	*(materials+3) = material(0.3f, 0.7f, 0.1f, 0.1f, 0.1f, 0.1f, 1.f, 1.f, 1.f, 1.f);
+	*(materials+4) = material(0.5f, 0.2f, 0.2f, 0.4f, 0.1f, 0.1f, 1.f, 1.f, 1.f, 10.f);
+
+	//TEMP link obj to materials
+	trimeshes->mat = materials;
+	(trimeshes + 1)->mat = (materials + 1);
+	(trimeshes + 2)->mat = (materials + 2);
+	(trimeshes + 3)->mat = (materials + 3);
+	(trimeshes + 4)->mat = (materials + 4);
+
+	//Set up Movement
+	//lets make the 2nd mesh move in a horizontal line
+	/*(trimeshes + 1)->doesMove = true;
+	(trimeshes + 1)->startMoveTime = 0.f;
+	(trimeshes + 1)->endMoveTime = 1.f;
+	(trimeshes + 1)->transformation = glm::vec3(1.f, 0.f, 0.f);*/
 
 	auto start_p0 = std::chrono::system_clock::now();
 
